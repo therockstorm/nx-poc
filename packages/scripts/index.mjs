a21f76c853e8359d9a25253f22bd30188748c80e
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-12-28 12:42:19 -0700

    Format

--- packages/scripts/index.mjs
+++ packages/scripts/index.mjs
@@ -1,13 +1,9 @@
 #!/usr/bin/env zx
-import 'zx/globals'
+import "zx/globals";
 
-await $`cat package.json | grep name`
+await $`cat package.json | grep name`;
 
-await Promise.all([
-  $`sleep 1; echo 1`,
-  $`sleep 2; echo 2`,
-  $`sleep 3; echo 3`,
-])
+await Promise.all([$`sleep 1; echo 1`, $`sleep 2; echo 2`, $`sleep 3; echo 3`]);
 
-let dir = 'fooBar'
-await $`mkdir /tmp/${dir}`
+let dir = "fooBar";
+await $`mkdir /tmp/${dir}`;
