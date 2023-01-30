227bb9cde109b6856ea377b156e508f5a324908d
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2023-01-30 13:01:29 -0700

    chore: stargazers

--- libs/scripts/github.mjs
+++ libs/scripts/github.mjs
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-// import { Octokit } from "@octokit/rest";
+import { Octokit } from "@octokit/rest";
 
-// const octokit = new Octokit({
-//   auth: "YOUR_ACCESS_KEY",
-// });
+const octokit = new Octokit({
+  auth: "",
+});
 
 // console.log(
 //   (
@@ -18,7 +18,10 @@
 // const REPO = "hi";
 // const BRANCH = "main";
 
-// // https://octokit.github.io/rest.js/v18#repos-create-for-authenticated-user
+const data = await getPaginatedData("/repos/mprimi/portable-secret/stargazers");
+console.log(data[0], data.length);
+
+// https://octokit.github.io/rest.js/v18#repos-create-for-authenticated-user
 // await octokit.repos.createInOrg({
 //   allow_auto_merge: true,
 //   allow_merge_commit: false,
@@ -71,3 +74,55 @@
 //     })
 //   ).data.map((d) => d.name)
 // );
+
+async function getPaginatedData(url) {
+  const nextPattern = /(?<=<)([\S]*)(?=>; rel="Next")/i;
+  let pagesRemaining = true;
+  let data = [];
+
+  while (pagesRemaining) {
+    const response = await octokit.request(`GET ${url}`, {
+      per_page: 100,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+
+    const parsedData = parseData(response.data);
+    data = [...data, ...parsedData];
+
+    const linkHeader = response.headers.link;
+
+    pagesRemaining = linkHeader && linkHeader.includes(`rel=\"next\"`);
+
+    if (pagesRemaining) {
+      url = linkHeader.match(nextPattern)[0];
+    }
+  }
+
+  return data;
+}
+
+function parseData(data) {
+  // If the data is an array, return that
+  if (Array.isArray(data)) {
+    return data;
+  }
+
+  // Some endpoints respond with 204 No Content instead of empty array
+  //   when there is no data. In that case, return an empty array.
+  if (!data) {
+    return [];
+  }
+
+  // Otherwise, the array of items that we want is in an object
+  // Delete keys that don't include the array of items
+  delete data.incomplete_results;
+  delete data.repository_selection;
+  delete data.total_count;
+  // Pull out the array of items
+  const namespaceKey = Object.keys(data)[0];
+  data = data[namespaceKey];
+
+  return data;
+}
